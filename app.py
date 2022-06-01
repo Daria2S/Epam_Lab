4ab23cddae04009e34776a3df6b1627b438e8025
Daria Savina <daria_savina@epam.com>
2022-06-01 21:45:40 +0300

    new comm

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Daria!"
+    return '<h1>Hi everyone. Want to present my app !</h1>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, threaded=True, debug=True)
